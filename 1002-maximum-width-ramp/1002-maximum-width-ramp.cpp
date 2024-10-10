5751bfb20fc45656c8adcd8a8ed1f42b231ef15d
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2024-10-11 02:38:13 +0530

    Time: 51 ms (48.83%), Space: 47 MB (25.09%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) 
+    {
+        stack<int>s;
+        int res = 0;
+        for(int i=0;i<nums.size();i++)
+        if(s.empty() || nums[s.top()] > nums[i])
+            s.push(i);
+
+        for(int i= nums.size()-1;i>res;i--)
+        {
+            while(s.size() and nums[s.top()] <= nums[i])
+            res = max(res,i-s.top()),s.pop();
+        }       
+        return res;
+    }
+};

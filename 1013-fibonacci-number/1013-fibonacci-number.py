18e6102d8799abe5a92f7cbd31b92173bf733998
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2024-10-22 17:39:04 +0530

    Time: 16 ms (58.48%), Space: 11.4 MB (87.97%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,8 +1,14 @@
 class Solution(object):
+    dict = {
+        0:0,
+        1:1
+    }
     def fib(self, n):
-        if n<=1:
-            return n
         
-        return self.fib(n-1) + self.fib(n-2)
+        if n in self.dict:
+            return self.dict[n]
+        
+        self.dict[n] = self.fib(n-1) + self.fib(n-2)
+        return self.dict[n]
     
     

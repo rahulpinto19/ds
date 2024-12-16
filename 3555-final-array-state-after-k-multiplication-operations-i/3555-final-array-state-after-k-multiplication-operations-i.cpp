8903256251a33c236735ad009b73b61a96a6da49
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2024-12-16 19:27:48 +0530

    Time: 0 ms (100%), Space: 27.3 MB (38.94%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    struct ComparePair {
-    bool operator()(const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
-        if (p1.first == p2.first) {
-            return p1.second > p2.second; // If first elements are equal, compare by second element
-        }
-        return p1.first > p2.first; // Otherwise, compare by first element
-    }
-};
-
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) 
     {
-            std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, ComparePair> pq;
-        int ind = 0;
-        for(auto x:nums)
+        int n = nums.size();
+        while(k--)
         {
-            pq.push({x,ind++});
-        }
-        while(k)
-        {
-            pair<int,int> temp = pq.top();
-            pq.pop();
-            pq.push({temp.first*multiplier,temp.second});
-            k--;
-        }
-        vector<int>ans(nums.size());
-        while(pq.size())
-        {
-            ans[pq.top().second] = pq.top().first;
-            pq.pop();
-        }
-        return ans;
+            int minIndex = 0;
+            for(int i=0;i<n;i++)
+            {
+                if(nums[minIndex]>nums[i])
+                {
+                    minIndex = i;
+                }
+            }
+            nums[minIndex]*=multiplier;
+        }    
+        return nums;
     }
 };

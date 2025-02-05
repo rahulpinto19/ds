6abd7de42cb1f298e8fc15ec3699f40cbc610b5c
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2025-02-05 22:18:38 +0530

    Time: 98 ms (73.64%), Space: 95.2 MB (92.96%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -5,25 +5,22 @@ public:
         unordered_map<int,int>mp;
         long long ans = 0;
         long long sum = 0;
-        for(int i=0;i<k;i++)
-        {
-            sum+=nums[i];
-            mp[nums[i]]++;
-        }
-        if(mp.size() == k)
-        ans = sum;
+
         int start = 0;
-        for(int i=k;i<nums.size();i++)
+        for(int i=0;i<nums.size();i++)
         {
             mp[nums[i]]++;
-            mp[nums[start]]--;
             sum+=nums[i];
-            sum-=nums[start];
-            if(mp[nums[start]] == 0)
+
+            if(i>=k)
             {
+                mp[nums[start]]--;
+                sum-=nums[start];
+                
+                if(mp[nums[start]] == 0)
                 mp.erase(nums[start]);
+                start++;
             }
-            start++;
             if(mp.size() == k)
                 ans = max(ans,sum);
         }

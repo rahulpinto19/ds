c81d93ca132e6d7edd827b353b856523d1366a19
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2025-02-14 09:31:07 +0530

    Time: 15 ms (3.91%), Space: 19.9 MB (60.53%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,43 +2,32 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
-        //    int num1=-1,num2=-1,count1=0,count2=0;
-    int n1 = -1,n2 = -1,cnt1 = 0,cnt2 = 0;
+        int ans1 = -1,ans2 = -1,cnt1 = 0,cnt2 = 0;
         for(auto x:nums)
         {
-            if(x == n1)    cnt1++;
-            
-            else if(x == n2) cnt2++;
-
+            if(ans1 == x)
+                cnt1++;
+            else if (ans2 == x)
+                cnt2++;
             else if(cnt1 == 0)
-            {
-                n1 = x;
-                cnt1 = 1;
-            }
+                {ans1 = x;cnt1 =1;}
             else if(cnt2 == 0)
-            {
-                n2 = x;
-                cnt2 = 1;
-            }
+                {ans2 = x;cnt2 = 1;}
             else
-            {
-                cnt1 --;
-                cnt2--;
-            }
-        }
-        cnt1=0,cnt2=0;
+                {cnt1--;cnt2--;}
+        }    
+        cnt1 = 0;cnt2 = 0;
         for(auto x:nums)
         {
-            if(n1 ==x)
-            cnt1++;
-            else if (n2 ==x)
-            cnt2++;
+            if(ans1 == x)
+                cnt1++;
+            else if(ans2  == x)
+                cnt2++;
         }
         vector<int>ans;
-        if(cnt1> nums.size()/3)
-            ans.push_back(n1);
-        if(cnt2>nums.size()/3)
-            ans.push_back(n2);
+        if(cnt1 > nums.size()/3) ans.push_back(ans1);
+        if(cnt2 > nums.size()/3) ans.push_back(ans2);
         return ans;
+
     }
 };

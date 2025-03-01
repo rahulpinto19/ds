6fd310ff442e04690f28c848c6cb7e3725e644e7
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2025-03-01 21:39:42 +0530

    Time: 80 ms (0%), Space: 11.4 MB (33.88%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.c
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.c
@@ -1,31 +1,26 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* applyOperations(int* nums, int numsSize, int* returnSize)
-{
-int i,j;
-for(i=0;i<numsSize-1;i++)
-{
-    if(nums[i]==nums[i+1])
-    {
-        nums[i]*=2;
-        nums[i+1]=0;
+int* applyOperations(int* nums, int numsSize, int* returnSize) {
+    int i, j;
+    for (i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        }
     }
-}
-*returnSize=numsSize;
-int pos=numsSize-1;
-int k;
-    for(k=0;k<10;k++)
-   { for(i=0;i<numsSize;i++)
-    {
-        if(nums[i]==0)
-        {for(j=i;j<numsSize-1;j++)
-        {
-            nums[j]=nums[j+1];
-            
+    *returnSize = numsSize;
+    int pos = numsSize - 1;
+    int k;
+    for (k = 0; k < 10; k++) {
+        for (i = 0; i < numsSize; i++) {
+            if (nums[i] == 0) {
+                for (j = i; j < numsSize - 1; j++) {
+                    nums[j] = nums[j + 1];
+                }
+                nums[j] = 0;
+            }
         }
-        nums[j]=0;
-        }
-    }}
-return nums;
+    }
+    return nums;
 }

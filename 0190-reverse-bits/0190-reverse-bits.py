91bfdfc1f1aeb3455a4c0b2ba06bbc39f15e4b77
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2025-11-14 00:47:56 +0530

    Time: 16 ms (62.04%), Space: 12.4 MB (49.98%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseBits(self, n):
+
+        res = 0
+        for _ in range(32):
+            res<<=1
+            res|=(n&1)
+            n>>=1
+        return res
+        

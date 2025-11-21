280282075cbbdbbcf81243fe18bfc355acaca7e9
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2025-11-21 15:05:38 +0530

    Time: 371 ms (20.45%), Space: 16.3 MB (29.44%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    int countPalindromicSubsequence(string s) 
-    {
-        unordered_map<int,vector<int>>mp;
-        for(int i=0;i<s.size();i++)
-        {
-            if(mp.find(s[i])==mp.end())
-            {
-                mp[s[i]].push_back(i);
-            }
+    int countPalindromicSubsequence(string s) {
+        
+        vector<int>first(26,-1),last(26,-1);
+
+        for(int i = 0;i<s.size();i++) {
+
+            if(first[s[i]-'a'] == -1)
+                first[s[i]-'a'] = i;
+            last[s[i]-'a'] = i;
         }
-        for(int i=s.size()-1;i>=0;i--)
-        {
-            if(mp[s[i]].size()==1)
-            {
-                mp[s[i]].push_back(i);
+        int ans = 0;
+        for(int i = 0;i<26;i++) {
+            set<int>st;
+            for(int j = first[i]+1;j<last[i];j++) {
+                st.insert(s[j]);
             }
+            ans+=st.size();
         }
-        int result =0;
-        for(auto x:mp)
-        {
-            vector<int>temp = x.second;
-            if(temp[1]-temp[0]>1)
-            {
-                set<int>st;
-                for(int i=temp[0]+1;i<s.size() and i<temp[1];i++)
-                {
-                    st.insert(s[i]);
-                }
-                result+=st.size();
-            }
-        }
-        return result;
+        return ans;
     }
 };

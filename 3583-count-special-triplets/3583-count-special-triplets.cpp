74bfe51facefdc418236431818ce866c6e4a820f
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2025-12-09 10:40:17 +0530

    Time: 580 ms (71.9%), Space: 352.2 MB (75.55%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
         
-        const int mod = 1e9 + 7;
+        int M = 1e9 + 7;
+        int n = nums.size();
+        unordered_map<int,int>freqPrev,freqNext;
 
-        unordered_map<int,int>prev,next;
-        for(auto x:nums)next[x]++;
+        vector<pair<int,int>>curr(n);
+        curr[0] = {0,0};
+        curr[n-1] = {0,0};
+        
         long long res = 0;
-
-        for(auto x:nums) {
-            next[x]--;
-            res = (res + ((long long )(prev[x*2]) * next[2*x] % mod))%mod;
-            prev[x]++;
+        
+        freqPrev[nums[0]]++;
+        freqNext[nums[n-1]]++;
+        
+        for(int i = 1;i<n-1;i++) {
+            
+            if(freqPrev.find(nums[i]*2) != freqPrev.end()) {
+                curr[i].first = freqPrev[nums[i]*2];
+            }
+            if(freqNext.find(nums[n-1-i]*2) != freqNext.end()) {
+                curr[n-1-i].second = freqNext[nums[n-1-i]*2];
+            }
+            
+            freqPrev[nums[i]]++;
+            freqNext[nums[n-1-i]]++;
         }
-        return res;
+        for(auto x:curr) {
+       
+        res += ((long long)x.second * x.first) % M;
+        }
+        return (res + M)%M;
     }
 };
